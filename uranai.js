0c68b428632d18e92a8cbd3ea3de8c3a7c20d5d2
jjs-0006 <jjs-0006@outlook.jp>
2014-05-20 16:00:27 +0900

    first

--- uranai.js
+++ uranai.js
@@ -1,31 +1,26 @@
 window.onload = function(){
-	var num = Math.random() * 5;
-	num = Math.floor(num);
+	var num = Math.floor(Math.random() * 5);
+	var res = "";
 	switch(num){
 		case 0:
-			document.getelementById(result).innerText = "大凶";
-			document.getelementById(note).innerText = "大凶";
+			res = "大吉";
 			break;
 		case 1:
-			document.getelementById(result).innerText = "末吉";
-			document.getelementById(note).innerText = "末吉";
+			res = "末吉";
 			break;
 		case 2:
-			document.getelementById(result).innerText = "吉";
-			document.getelementById(note).innerText = "吉";
+			res = "吉";
 			break;
 		case 3:
-			document.getelementById(result).innerText = "中吉";
-			document.getelementById(note).innerText = "中吉";
+			res = "中吉";
 			break;
 		case 4:
-			document.getelementById(result).innerText = "大吉";
-			document.getelementById(note).innerText = "大吉";
+			res = "大吉";
 			break;
 		default:
-			document.getelementById(result).innerText = "平";
-			document.getelementById(note).innerText = "平";
+			
 			break;
 	}
+	document.getElementById("result").innerText = res;
 
 }
